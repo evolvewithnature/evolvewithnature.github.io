ae3647d6aba1c16486fbe74f9c366eb08c4d1b7f
sharadshriram <sharad.sriram@gmail.com>
2018-02-08 15:46:47 +0100

    fixed wrong mail id - added field for purpose

--- js/sendMail.js
+++ js/sendMail.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
   $('#contact-form').submit(function(e) {
     var name    = document.getElementById('inputName')
     var email   = document.getElementById('inputEmail')
+    var purpose   = document.getElementById('selectPurpose')
     var message = document.getElementById('inputMessage')
 
-    if (!name.value || !email.value || !message.value) {
+    if (!name.value || !email.value || !purpose.value || !message.value) {
       alertify.error("Please check your entries");
       return false;
     } else {
       $.ajax({
         method: 'POST',
-        url: '//formspree.io/evolvewithnature@gmail.com',
+        url: '//formspree.io/evolvewithnature2@gmail.com',
         data: $('#contact-form').serialize(),
         datatype: 'json'
       });
