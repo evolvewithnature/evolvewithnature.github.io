30e4adaa5e84333049748f97d759c42a39b13bbf
sharadshriram <sharad.sriram@gmail.com>
2018-02-11 14:50:56 +0100

    fixed navigation for mobile

--- js/index.js
+++ js/index.js
@@ -1,24 +1,4 @@
-//Sending emails using the formspree API
-$(document).ready(function() {
-  $('#contact-form').submit(function(e) {
-    var name    = document.getElementById('inputName')
-    var email   = document.getElementById('inputEmail')
-    var purpose   = document.getElementById('selectPurpose')
-    var message = document.getElementById('inputMessage')
-
-    if (!name.value || !email.value || !purpose.value || !message.value) {
-      alertify.error("Please check your entries");
-      return false;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '//formspree.io/evolvewithnature2@gmail.com',
-        data: $('#contact-form').serialize(),
-        datatype: 'json'
-      });
-      e.preventDefault();
-      $(this).get(0).reset();
-      alertify.success("Message sent");
-    }
-  });
+/*collapse after touch*/
+$('.nav a').on('click', function(){
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
